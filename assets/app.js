85f421954efb4f0a51a792f047ed116169b56054
Joel Buentello <joel.buentello@yahoo.com>
2019-09-12 08:51:43 -0500

    Was able to get the database to connect, I am removing the js file to seperate js file like it was previoulsy.'

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,45 @@
 $(document).ready(function() {
    
-      // Your web app's Firebase configuration
-      var firebaseConfig = {
-        apiKey: "AIzaSyBOlGrJturWavBdtVEWa33ABqiAmsi7MtQ",
-        authDomain: "train-scheduler-72bab.firebaseapp.com",
-        databaseURL: "https://train-scheduler-72bab.firebaseio.com",
-        projectId: "train-scheduler-72bab",
-        storageBucket: "train-scheduler-72bab.appspot.com",
-        messagingSenderId: "199082199417",
-        appId: "1:199082199417:web:337a2e80b62f4e411807a8"
-      };
+    //   // Your web app's Firebase configuration
+    //   var firebaseConfig = {
+    //     apiKey: "AIzaSyBOlGrJturWavBdtVEWa33ABqiAmsi7MtQ",
+    //     authDomain: "train-scheduler-72bab.firebaseapp.com",
+    //     databaseURL: "https://train-scheduler-72bab.firebaseio.com",
+    //     projectId: "train-scheduler-72bab",
+    //     storageBucket: "train-scheduler-72bab.appspot.com",
+    //     messagingSenderId: "199082199417",
+    //     appId: "1:199082199417:web:337a2e80b62f4e411807a8"
+    //   };
       
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+    //   // Initialize Firebase
+    //   firebase.initializeApp(firebaseConfig);
 
-      var db = firebase.database();
+    //   var db = firebase.database();
+    //   console.log(db)
 
-      $("#newTrain").on("click", function(event) {
-        event.preventDefault();
+//       $("#newTrain").on("click", function(event) {
+//         event.preventDefault();
 
-      //Add Variables to be able to grab the data that is entered
-      var Name = $(#trainName-input).val()
-      var TrainOne = $("#trainOne-input").val()
-      var Destination = $("#destination-input").val()
-      var Freq = $("#freq-input").val()
+//       //Add Variables to be able to grab the data that is entered
+//       var Name = $(#trainName-input).val()
+//       var TrainOne = $("#trainOne-input").val()
+//       var Destination = $("#destination-input").val()
+//       var Freq = $("#freq-input").val()
 
-      var TrainData= {
-          Name: Name,
-          Destination: Destination,
-          Start: TrainOne,
-          Frequency: Freq 
-      };
+//       var TrainData= {
+//           Name: Name,
+//           Destination: Destination,
+//           Start: TrainOne,
+//           Frequency: Freq 
+//       };
 
-      //Push TrainData to the database
-      database.ref().push(TrainData);
-}:
+//       //Push TrainData to the database
+//       database.ref().push(TrainData);
+
+//       //Figure out how to erase items from the databoxes 
+//       $("#trainName-input").val("");
+// 	  $("#destination-input").val("");
+// 	  $("#TrainOne-input").val("");
+// 	  $("#freq-input").val("");
+// });
+
